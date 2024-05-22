66ce6f000c0ab4a9187b28301db7529748a59c56
Drazor153 <fabianjusto2341@gmail.com>
2024-05-21 20:04:52 -0400

    increase limit body request

--- server/src/app.js
+++ server/src/app.js
@@ -6,14 +6,13 @@ import galleryRoutes from "../routes/gallery.routes";
 const app = express();
 
 var cors = require("cors");
-
 app.use(
   cors({
     origin: '*'
   })
 );
 app.set("port", config.port);
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(galleryRoutes);
 
 export default app;
